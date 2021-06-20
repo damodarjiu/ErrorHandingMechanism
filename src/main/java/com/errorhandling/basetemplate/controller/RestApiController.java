6cc68395400431a42cc77a97b7f76ee921607fd4
damod <roy.cse1416@gmail.com>
2021-06-20 13:42:45 +0530

    LockBack impl.

--- src/main/java/com/errorhandling/basetemplate/controller/RestApiController.java
+++ src/main/java/com/errorhandling/basetemplate/controller/RestApiController.java
@@ -4,6 +4,8 @@ import com.errorhandling.basetemplate.constants.ErrorEnum;
 import com.errorhandling.basetemplate.exceptions.BadRequestException;
 import com.errorhandling.basetemplate.exceptions.ResourceAccesException;
 import com.errorhandling.basetemplate.exceptions.UnAuthorizeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -13,8 +15,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/error-api")
 public class RestApiController {
 
+    private static final Logger logger = LoggerFactory.getLogger(RestApiController.class);
+
     @GetMapping(value = "/errorcode/{code}", produces = "application/json")
     public Object getData(@PathVariable String code) {
+        logger.info("@@ Started of getData Method @@");
         if (code.equals("400"))
             throw new BadRequestException(ErrorEnum.BS_CL_BAD_REQUEST.getMessage());
         if (code.equals("401"))
@@ -23,6 +28,7 @@ public class RestApiController {
         if (code.equals("404"))
             throw new ResourceAccesException(ErrorEnum.BS_CL_NOT_AUTHORIZE.getMessage());
 
+        logger.info("@@ Ending of getData Method @@");
         return "Hello";
     }
 
