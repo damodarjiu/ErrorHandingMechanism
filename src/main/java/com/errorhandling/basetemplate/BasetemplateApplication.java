844b054215647fb1c9c3488c90502472b7c8b88c
damod <roy.cse1416@gmail.com>
2021-06-20 10:38:41 +0530

    Intial Commit

---
+++ src/main/java/com/errorhandling/basetemplate/BasetemplateApplication.java
@@ -0,0 +1,13 @@
+package com.errorhandling.basetemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasetemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasetemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/errorhandling/basetemplate/BasetemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.errorhandling.basetemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasetemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
